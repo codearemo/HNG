e5c894174fa0a4443e1cf3491604dc31d2f377ff
cypher12 <busola.omosipe@gmail.com>
2018-04-12 08:36:26 +0100

    Hello HNG world...this is my coded stage 1 project

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HNG Internship</title>
+  <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+  <header id="header">
+    <h2>Imagine more...</h2>
+  </header>
+  <section id="profile">
+    <div id="pic-hold">
+      <img src="../img/myAvatar.png" alt="My Avatar Picture">
+    </div>
+    <div id="text-section">
+      <h3 id="text-header">This is me</h3>
+      <p>Omosipe Busola? Designer Developer, the right guy. Just another developer with a few more logic. Want to get in touch with me? 
+        Be it to request more info about myself or my experience, to ask for my resume, tips on how to solve your sudoku, 
+        random questions about the universe and the meaning of life, 
+        or even if only for some info on how nice the weather is here... just feel free to drop me a line anytime.</p>
+      <h1><?php date_default_timezone_set('Africa/Lagos'); echo date("H:i:s");?></h1>
+    </div>
+  </section>
+</body>
+</html>
